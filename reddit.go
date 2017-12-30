20a486ead7304f29d8b9b7a43044430c3a12b9a3
Mike Mikulak <mpmikulak@gmail.com>
2017-12-30 16:44:44 -0600

    added random and top post options

--- reddit.go
+++ reddit.go
@@ -2,10 +2,41 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"strings"
+	"time"
 
+	"github.com/reujab/wallpaper"
 	"github.com/turnage/graw/reddit"
 )
+
+func randomPost(posts []*reddit.Post) (post *reddit.Post) {
+	rand.Seed(int64(time.Now().Second()))
+	return posts[rand.Intn(len(posts))]
+}
+
+func sortedPost(posts []*reddit.Post) (post *reddit.Post) {
+	post = posts[0]
+	for x := range posts {
+		if posts[x].Ups >= post.Ups {
+			post = posts[x]
+		}
+	}
+	return post
+}
+
+func setWallpaper(post *reddit.Post) error {
+	url := post.URL
+	if strings.HasPrefix(url, "https://imgur.com/") {
+		url = strings.TrimPrefix(url, "https://imgur.com/")
+		url = "https://i.imgur.com/" + url + ".jpg"
+	}
+	err := wallpaper.SetFromURL(url)
+	if err != nil {
+		return err
+	}
+	return nil
+}
 
 func main() {
 	// Initialize the bot, using the credintials from a local file
@@ -14,33 +45,21 @@ func main() {
 		panic(err)
 	}
 
-	// Retrieve a harvest from a specific reddit
-	harvest, err := bot.Listing("/r/wallpapers", "")
-	if err != nil {
-		panic(err)
-	}
-
-	var post *reddit.Post
-	for x := range harvest.Posts {
-		if x <= 0 {
-			post = harvest.Posts[x]
-		} else {
-			if harvest.Posts[x].Ups >= post.Ups {
-				post = harvest.Posts[x]
-			} else {
-				continue
-			}
+	// Retrieve a harvest from the wallpaper reddit
+	for {
+		fmt.Println("Polling r/wallpapers")
+		harvest, err := bot.Listing("/r/wallpapers", "")
+		if err != nil {
+			panic(err)
 		}
+		posts := harvest.Posts
+
+		post := randomPost(posts)
+
+		err = setWallpaper(post)
+		if err != nil {
+			fmt.Println(err)
+		}
+		time.Sleep(15 * time.Second)
 	}
-
-	if strings.HasPrefix(post.URL, "https://imgur.com/StsRgCA") {
-
-	}
-	fmt.Println(post.URL)
-	// Set the wallpaper as the URL aquired from the sorted posts
-	// err = wallpaper.SetFromURL(post.URL)
-	// if err != nil {
-	// 	panic(err)
-	// }
-
 }
